0c5907c4bcf63a1d0f1a745ad5d0365e8dd0ff83
Grimnack <matthieu.caron@laposte.net>
2017-01-07 12:09:57 +0100

    Pareto fini, script todo

--- OC_part2/src/algos/ParetoLocalSearch.java
+++ OC_part2/src/algos/ParetoLocalSearch.java
@@ -1,9 +1,7 @@
 package algos;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Random;
 
 import probleme.Probleme;
 
@@ -15,11 +13,10 @@ public class ParetoLocalSearch {
 	protected boolean first ;
 	
 	
-	public ParetoLocalSearch(Probleme probleme, Voisinage voisinage,Solution solutionInitiale, boolean first){
+	public ParetoLocalSearch(Probleme probleme, Voisinage voisinage,Solution solutionInitiale){
 		this.probleme = probleme ;
 		this.voisinage = voisinage ;
 		this.pareto.add(solutionInitiale);
-		this.first = first ;
 	}
 	
 	private boolean domine(int[] eval1, int[] eval2){
@@ -33,19 +30,44 @@ public class ParetoLocalSearch {
 		return toujoursInfouEgal && (eval1 != eval2) ;
 	}
 	
+	public Solution randomSelect(ArrayList<Solution> pareto){
+		Collections.shuffle(pareto);
+		for (Solution solution : pareto) {
+			if(!solution.visited){
+				return solution ;
+			}
+		}
+		return null;
+	}
+	
 	public ArrayList<Solution> run(){
 		while(true){
 			// On selectionne au hasard une solution du pareto
-			Collections.shuffle(pareto);
-			Solution solutionActuelle = pareto.remove(0);
+			Solution solutionActuelle = randomSelect(pareto);
+			if(solutionActuelle == null){
+				break; // on a visité toutes les solutions.
+			}
 			this.voisinage.init(solutionActuelle);
+			solutionActuelle.setVisitedTrue();
 			while(this.voisinage.hasnext()){
 				Solution solutionCandidate =(Solution) voisinage.next();
+				int[] evalCandidat = this.probleme.eval(solutionCandidate);
+				boolean dominated = false ;
+				for (Solution solution : pareto) {
+					int[] evalPareto = this.probleme.eval(solution) ;
+					if(domine(evalCandidat,evalPareto)){
+						this.pareto.remove(solution);
+					}else if(domine(evalPareto,evalCandidat)){
+						dominated = true ;
+						break ;
+					}
+				}
+				if(!dominated){
+					pareto.add(solutionCandidate);
+				}
 				
 			}
 		}
-		
-		
-		return null;
+		return pareto;
 	}
 }
